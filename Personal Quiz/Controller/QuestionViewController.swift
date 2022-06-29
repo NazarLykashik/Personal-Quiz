a69ba9723058c3b10487380d0bb5623809317060
NazarLukashik <nazarlykashik@gmail.com>
2022-06-29 13:17:48 +0300

    29.06.2022 start

--- Personal Quiz/Controller/QuestionViewController.swift
+++ Personal Quiz/Controller/QuestionViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class QuestionViewController: UIViewController {
     
+    //MARK: - IB Outlets
+    
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var singleStackView: UIStackView!
     @IBOutlet var singleButtons: [UIButton]!
@@ -20,23 +22,59 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var rangedStackView: UIStackView!
     @IBOutlet var rangedLables: [UILabel]!
     @IBOutlet weak var rangedSlider: UISlider!
+    
+    @IBOutlet weak var qeustionProgreView: UIProgressView!
+    
+    //MARK: Private Properties
+    
     private let questions = Question.getQuestions()
+    private var questionIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        uppdateIU()
 
-        // Do any additional setup after loading the view.
+
     }
+ 
+    //MARK: Private Metods
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //Uppdate user interface
+    private func uppdateIU(){
+        //Hide evrythyng
+        for stackView  in [singleStackView, multipleStackView,rangedStackView]{
+                stackView?.isHidden = true
+        }
+        //get current question
+        let currentQuestion = questions[questionIndex]
+        // Set current question for current qestion
+        questionLabel.text = currentQuestion.text
+        //  calculate progress
+        let totalProgress = Float(questionIndex / questions.count)
+        // Set progress for question progress view
+        qeustionProgreView.setProgress(totalProgress, animated: true)
+        //  set navigation title
+        title = "Вопрос № \(questionIndex + 1)  из \(questions.count)"
+        
+        let currentAnswers = currentQuestion.ansvers
+        //show stack view corresponding to question type
+        switch currentQuestion.type{
+        case .single:
+            updateSingleStackView(using: currentAnswers)
+        case .multiple:
+            break
+        case .ranged:
+            break
+            
+        }
     }
-    */
-
+    /// setup single stack view
+    ///
+    /// - Parameter answers: - array with answers
+    ///
+    /// description
+    private func updateSingleStackView(using answers: [Answer]){
+        // show single stack view
+        singleStackView.isHidden = false
+    }
 }
